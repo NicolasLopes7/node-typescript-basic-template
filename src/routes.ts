e7a696bd5933b24ec6f2933e8b21ed1ee0608173
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-06-14 21:46:50 -0300

    Initial Commit

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import UserController from '~/Controllers/UserController';
-
-const routes = Router();
-
-routes.get('/', UserController.index);
-
-export default routes;
--- src/server.ts
+++
@@ -1,3 +0,0 @@
-import app from './App';
-
-app.listen(3333);
